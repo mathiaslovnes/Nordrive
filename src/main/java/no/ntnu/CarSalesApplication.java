7c0a4f5ca06ba0231964693f5c320a752e00cbdb
Mathias Løvnes <mathiaslovnes02@gmail.com>
2026-03-31 23:31:49 +0200

    Initial project setup
    Including gitignore, pom.xml, main project directories and application
    entry point.

---
+++ src/main/java/no/ntnu/CarSalesApplication.java
@@ -0,0 +1,11 @@
+package no.ntnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSalesApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(CarSalesApplication.class, args);
+  }
+}
